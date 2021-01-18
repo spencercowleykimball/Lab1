e4c15fe25b53d7a2de9751b713163cd07a9f9382
Spencer Kimball <spencercowleykimball@gmail.com>
2021-01-18 17:29:50 +0000

    Initial commit

---
+++ Lab1/TodoList.h
@@ -0,0 +1,68 @@
+#ifndef TODO_LIST_H
+#define TODO_LIST_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "TodoListInterface.h"
+
+using namespace std;
+
+class TodoList: public TodoListInterface {
+public:
+  vector <string> tasks;
+  TodoList() {
+      cout << "In Constructor" << endl;
+      ifstream infile ("TODOList.txt");
+      string line;
+      if (infile.is_open())
+      {
+        while (getline(infile, line))
+        {
+          cout << line << "\n";
+          tasks.push_back(line);
+        }
+        infile.close();
+      }
+  }
+  virtual ~TodoList() {
+      cout << "In Destructor" << endl;
+      ofstream outfile;
+      outfile.open ("TODOList.txt", ofstream::out | ofstream::trunc);
+      for(int i = 0; i < tasks.size(); i++) {
+        cout << tasks[i] << "\n";
+        outfile << tasks[i] << endl;
+      }
+      outfile.close();
+  } 
+  
+  /*virtual ~TodoListInterface() {}    /*    *   Adds an item to the todo list with the data specified by the string "_duedate" and the task specified by "_task"    */    
+  
+  virtual void add(string _duedate, string _task) {
+    cout << "In add " << _duedate<< " " << _task << endl;
+    tasks.push_back(_duedate+" "+_task);
+  }    
+  
+  /*    
+  *   Removes an item from the todo list with the specified task name    
+  *    
+  *   Returns 1 if it removes an item, 0 otherwise */    
+  
+  virtual int remove(string _task) 
+  {
+    cout << "In Remove" << endl;
+  }   /*    *   Prints out the full todo list to the console    */    
+  
+  virtual void printTodoList()
+  {
+    cout << "In List" << endl;
+  }    /*    *   Prints out all items of a todo list with a particular due date (specified by _duedate)    */    
+  
+  virtual void printDaysTasks(string _date)
+  {
+    cout << "In daystasks" << endl;
+  }
+
+};
+#endif
---
+++ Lab1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "TodoList.h"
+using namespace std;
+int main(int argc, char *argv[]) 
+{
+  TodoList mylist;
+  for(int i = 0; i < argc; i++) {
+    cout << "argv[" << i << "]=" << argv[i] << endl;
+  }
+  
+  string firstarg = argv[1];
+
+  if(firstarg.compare("add") == 0) {
+    cout << "Doing an add" << endl;
+    string date = argv[2];
+    string task = argv[3];
+    cout << "date " << date << " task " << task << endl;
+    mylist.add(date, task);
+
+  }
+}
