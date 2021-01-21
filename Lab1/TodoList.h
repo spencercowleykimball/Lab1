5a5fcc559e0f5e5bcdbbceb715d484cae5f225f8
Spencer Kimball <spencercowleykimball@gmail.com>
2021-01-21 01:30:55 +0000

    almost finished

--- Lab1/TodoList.h
+++ Lab1/TodoList.h
@@ -38,8 +38,9 @@ public:
       outfile.close();
   } 
   
-  /*virtual ~TodoListInterface() {}    /*    *   Adds an item to the todo list with the data specified by the string "_duedate" and the task specified by "_task"    */    
-  
+  /*    
+	*   Adds an item to the todo list with the data specified by the string "_duedate" and the task specified by "_task"    
+	*/ 
   virtual void add(string _duedate, string _task) {
     cout << "In add " << _duedate<< " " << _task << endl;
     tasks.push_back(_duedate+" "+_task);
@@ -50,60 +51,57 @@ public:
   *    
   *   Returns 1 if it removes an item, 0 otherwise 
   */    
-  
   virtual int remove(string _task) 
   {
-		
+		int removed = 0;
     cout << "Here " << _task << endl;
     for (int i = 0; i < (tasks.size()); i++) {
 			string toFindTask = tasks.at(i);
 			cout << "tasks[" << i << "] " << tasks[i] << endl;
       if(toFindTask.find(_task) != string::npos) {
         tasks.erase(tasks.begin() + i);
+				removed = 1;
       }
     }
+
+		return removed;
   }   
   
   /*    
   *   Prints out the full todo list to the console    
   */    
-  
   virtual void printTodoList()
   {
     cout << "All tasks" << endl;
 		for (int i = 0; i < (tasks.size()); i++) {
-			cout << tasks[i] << endl;
-      
+			cout << tasks.at(i) << endl;
     }
-  }    
+		cout << endl;
+
+  } 
+
   /*    
   *   Prints out all items of a todo list with a particular due date (specified by _duedate)    
   */    
-  
-  virtual void printDaysTasks(string _day)
+  virtual void printDaysTasks(string _duedate)
   {
     cout << "In daystasks" << endl;
-    int stringLength = _day.size(); //Get length of input day
+    int stringLength = _duedate.size(); //Get length of input day
 
-    
-		cout << _day << endl;
+		cout << _duedate << endl;
 
 		for (int i = 0; i < (tasks.size()); i++) {
 			
 			string toFindTask = tasks.at(i);
       
-			if(!toFindTask.find(_day)) {
+			if(!toFindTask.find(_duedate)) {
 				string dayTasks = tasks[i];
 				dayTasks.erase(dayTasks.begin(), dayTasks.begin() + stringLength);
 				
 				cout << dayTasks << endl;
 			}
-
-      
-      
     }
-  
-	cout << "FINISHED" << endl;
+		cout << endl;
 
   }
 
