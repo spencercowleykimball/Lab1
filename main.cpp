409eaf6ac82f3fd4defbca4e7808c74adda13cc7
Spencer Kimball <spencercowleykimball@gmail.com>
2021-01-21 01:48:12 +0000

    Finished I think??

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ using namespace std;
 int main(int argc, char *argv[]) 
 {
   TodoList mylist;
-  
   for(int i = 0; i < argc; i++) {
     cout << "argv[" << i << "]=" << argv[i] << endl;
   }
   
   string firstarg = argv[1];
 
+  //For the add option
   if(firstarg.compare("add") == 0) {
     cout << "Doing an add" << endl;
     string date = argv[2];
@@ -22,8 +22,33 @@ int main(int argc, char *argv[])
     mylist.add(date, task);
 
   }
-}
 
-{
-  
+  //For the remove option
+	if(firstarg.compare("remove") == 0) {
+    cout << "Doing a remove" << endl;
+    string task = argv[2];
+
+    cout << " task " << task << endl;
+    mylist.remove(task);
+
+  }
+
+  //For the print all tasks option
+	if(firstarg.compare("printList") == 0) {
+    cout << "Printing all tasks" << endl;
+
+    mylist.printTodoList();
+
+  }
+
+  //For the print all tasks for a certain day option
+	if(firstarg.compare("printDay") == 0) {
+    string day = argv[2];
+    cout << "Printing tasks for " << day << endl;
+
+    cout << " tasks for " << day << ": " << endl;
+    mylist.printDaysTasks(day);
+
+  }
+
 }
